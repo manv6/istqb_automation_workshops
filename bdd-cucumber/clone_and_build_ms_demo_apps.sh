e8acd6ee7922d07c300d10c2c3cc4cba1f7d8097
Manolis Vlastos <evlastos@theorchard.com>
2018-06-01 12:28:02 +0300

    remove unused git repo

--- bdd-cucumber/clone_and_build_ms_demo_apps.sh
+++ bdd-cucumber/clone_and_build_ms_demo_apps.sh
@@ -7,7 +7,6 @@ git clone https://github.com/jaheikki/microservice-demo-eureka-server-copy.git
 git clone https://github.com/jaheikki/microservice-demo-zuul-server-copy.git
 git clone https://github.com/jaheikki/microservice-demo-customer-copy.git
 git clone https://github.com/jaheikki/microservice-demo-catalog-copy.git
-git clone https://github.com/jaheikki/microservice-demo-acceptance-tests-copy.git
 
 cd microservice-demo-order-copy/ && mvn clean install && cd ..
 cd microservice-demo-zuul-server-copy/ && mvn clean install && cd ..
