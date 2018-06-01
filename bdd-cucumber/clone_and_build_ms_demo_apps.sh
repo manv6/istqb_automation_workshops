c73be3d9c6fbe0aa168a6ffddd571cee92804b8d
Manolis Vlastos <evlastos@theorchard.com>
2018-06-01 12:21:00 +0300

    sample app for bdd workshop

---
+++ bdd-cucumber/clone_and_build_ms_demo_apps.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir -p git
+cd git
+git clone https://github.com/jaheikki/microservice-demo-order-copy.git
+git clone https://github.com/jaheikki/microservice-demo-eureka-server-copy.git
+git clone https://github.com/jaheikki/microservice-demo-zuul-server-copy.git
+git clone https://github.com/jaheikki/microservice-demo-customer-copy.git
+git clone https://github.com/jaheikki/microservice-demo-catalog-copy.git
+git clone https://github.com/jaheikki/microservice-demo-acceptance-tests-copy.git
+
+cd microservice-demo-order-copy/ && mvn clean install && cd ..
+cd microservice-demo-zuul-server-copy/ && mvn clean install && cd ..
+cd microservice-demo-eureka-server-copy/ && mvn clean install && cd ..
+cd microservice-demo-customer-copy/ && mvn clean install && cd ..
+cd microservice-demo-catalog-copy/ && mvn clean install && cd ..
---
+++ bdd-cucumber/run-app.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# cd git/microservice-demo-acceptance-tests-copy/src/test/resources
+
+docker-compose -f docker-compose-dev.yml up -d
